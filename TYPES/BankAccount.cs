e70adad5a3f4684642bc961350804a19c9572619
Daniel Svan <daniel_svan1@hotmail.com>
2022-11-28 21:45:50 +0100

    lade till constructor och klass för bankloan och bankaccount

---
+++ TYPES/BankAccount.cs
@@ -0,0 +1,14 @@
+class BankAccount
+{
+    int accountNumber {get; set;}
+    string accountType {get; set;}
+    decimal totalBalance {get; set;}
+
+    public BankAccount(int accountNumber, string accountType, decimal totalBalance)
+    {
+        this.accountNumber = accountNumber;
+        this.accountType = accountType;
+        this.totalBalance = totalBalance;
+
+    }
+}
