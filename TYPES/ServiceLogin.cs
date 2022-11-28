87d745f0b3e14735236e543d9a83a3b3d8dfbc75
Daniel Svan <daniel_svan1@hotmail.com>
2022-11-28 21:55:40 +0100

    Addera serviceLogin class och transactions class med constructorer. All är i delen/mappen TYPES

---
+++ TYPES/ServiceLogin.cs
@@ -0,0 +1,15 @@
+class ServiceLogin
+{
+    int employementNumber {get; set;}
+    string name {get; set;}
+    string lastName {get; set;}
+    string email {get; set;}
+
+    public ServiceLogin (int employementNumber, string name, string lastName, string email)
+    {
+        this.employementNumber = employementNumber;
+        this.name = name;
+        this.lastName = lastName; 
+        this.email = email;
+    }
+}
---
+++ TYPES/Transactions.cs
@@ -0,0 +1,4 @@
+class Transactions
+{
+    //Filura på om du ska separera eller ha en egen table för withdrawals, deposits, payments för enskild id tracking;
+}
