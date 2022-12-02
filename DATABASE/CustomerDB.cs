193ad51bf46873f1ca095e3d20804446f5797271
Daniel Svan <daniel_svan1@hotmail.com>
2022-12-02 16:10:32 +0100

    synkat git och lägger till addcustomer funktion till dabase

--- DATABASE/CustomerDB.cs
+++ DATABASE/CustomerDB.cs
@@ -4,14 +4,14 @@ using MySqlConnector;
 namespace DATABASE;
 class Customer
 {
+    public List <Customer> customer = new();
     public int AddCustomer()
     {
-        List <Customer> customer = new();
 
         using (MySqlConnection connection = new MySqlConnection ($"Server = localhost; Database = svans_bank;Uid=rood;Pwd=;"))
         {
             int rows = 0;
-            string query = "INSERT INTO customer(name, last_name, personal_number ,mail, phone_number, location, street_adress, street_number, postal_number )VALUES(@name,@SocialSecurityNumber,@email,@passWord);";
+            string query = "INSERT INTO customer(name, last_name, personal_number ,mail, phone_number, location, street_adress, street_number, postal_number, pass_word )VALUES(@name, @lastName, @personalNumber, @mail, @phoneNumber, @location, @streetAdress, @streetNumber, @postalNumber, @passWord);";
             rows = connection.ExecuteScalar<int>(query, param: customer);
             return rows;
         }
