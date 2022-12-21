8285f1779905a0f6f69f61557abb2a24fee99dac
Daniel Svan <daniel_svan1@hotmail.com>
2022-12-21 13:42:36 +0100

    Små ändringar

--- UI/CustomerPageOperator.cs
+++ UI/CustomerPageOperator.cs
@@ -10,9 +10,14 @@ public class CustomerPageOperator
         {
             int customerPage = 0;
             string answer = string.Empty;
-            while (true)
+            decimal amount = 0;
+            bool customerMainMenu = true;
+            bool approveTransaction = true;
+            bool disapproveTransaction = false;
+        
+            while (customerMainMenu)
             {
-                System.Console.WriteLine("Ditt saldo:");
+                System.Console.WriteLine("Ditt saldo:" );
                 System.Console.WriteLine("[1] Betala");
                 System.Console.WriteLine("[2] Överför egna konton");
                 System.Console.WriteLine("[3] Öppna nytt konto");
@@ -32,16 +37,22 @@ public class CustomerPageOperator
                 switch (customerPage)
                 {
                     case 1:
+                    
+                    
+                        System.Console.WriteLine("Tryck på q för att avbryta och återgå till startmenyn");
                         System.Console.WriteLine("Från konto: "); //Finns det fler konton Får det adderas
                         answer = Console.ReadLine();
                         System.Console.WriteLine("Till konto: ");
                         answer = Console.ReadLine();
                         System.Console.WriteLine("Belopp: ");
-                        decimal amount = decimal.Parse(Console.ReadLine());
+                        amount = decimal.Parse(Console.ReadLine());
                         System.Console.WriteLine("Översikt");
                         System.Console.WriteLine("Skapa en överblick av valda inskrivna alternativ innan godkännande");
                         System.Console.WriteLine("Godkänn Överföring /true/false");
+                
+
                         //If true begär transaktion
+                        
                         break;
                     case 2:
                         System.Console.WriteLine("Från konto");
@@ -55,29 +66,29 @@ public class CustomerPageOperator
                         System.Console.WriteLine("Godkänn Överföring /true/false");
                         break;
                     case 3:
-                    
-                    System.Console.WriteLine("Typ av konto som skall öppnas");
-                    System.Console.WriteLine("Sparande konto");
-                    System.Console.WriteLine("Överföring konto");
-                    //Överförings konto är för att överföra belopp i kortare tid/tillfälligt
-                    //Sparande konto är för att överföra belopp i längre perioder.
-                    System.Console.WriteLine("Öppna konto till. //Skapa inner join");
-                    answer = Console.ReadLine();
+
+                        System.Console.WriteLine("Typ av konto som skall öppnas");
+                        System.Console.WriteLine("Sparande konto");
+                        System.Console.WriteLine("Överföring konto");
+                        //Överförings konto är för att överföra belopp i kortare tid/tillfälligt
+                        //Sparande konto är för att överföra belopp i längre perioder.
+                        System.Console.WriteLine("Öppna konto till. //Skapa inner join");
+                        answer = Console.ReadLine();
                         break;
                     case 4:
-                    //Rad olika förfrågningar som krävs för att godkänna lån
-                    //Förfrågan skall skickas till Service_login för godkännade
-                    //If true == skapa ett lån godkännande 
-                    System.Console.WriteLine();
+                        //Rad olika förfrågningar som krävs för att godkänna lån
+                        //Förfrågan skall skickas till Service_login för godkännade
+                        //If true == skapa ett lån godkännande 
+                        System.Console.WriteLine();
                         break;
                     case 5:
-                    //Behöver göra separata statiska metoder för 4 och och 5.
+                        //Behöver göra separata statiska metoder för 4 och och 5.
                         break;
                     case 6:
-                     //Hämta konto information ifrån GetCustomer. 
-                     //Enbart 2 styck redigerar kan göras på telefon nr och mail.
-                     // Resten av ändringar skall begäras till customer_Service
-                     //Skapa felhantering i CreateAccount så enkla misstag ej skickas till DB.
+                        //Hämta konto information ifrån GetCustomer. 
+                        //Enbart 2 styck redigerar kan göras på telefon nr och mail.
+                        // Resten av ändringar skall begäras till customer_Service
+                        //Skapa felhantering i CreateAccount så enkla misstag ej skickas till DB.
                         break;
 
                 }
